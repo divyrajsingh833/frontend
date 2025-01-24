5a415ec6e7462879b190c163acc879445ed65f54
Divyraj Singh <divyrajsingh833@gmail.com>
2025-01-24 08:10:31 +0530

    Update axios.jsx

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL : 'http://localhost:3000/api'
+  baseURL : 'https://backend-alpha-ashy.vercel.app/'
 })
 
 export default instance
